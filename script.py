a7945ab1809947c06d669a116d6e60704eae24ad
lucyanodimiteiu <lucyano.dimitriu@gmail.com>
2026-01-18 15:16:42 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,11 +5,15 @@ import re
 from newspaper import Article
 from deep_translator import GoogleTranslator
 
-# Sursele tale
+# LISTA TA EXTINSĂ DE SURSE
 RSS_URLS = [
     "https://www.digi24.ro/rss", 
     "https://www.hotnews.ro/rss",
-    "https://www.marketwatch.com/rss/topstories"
+    "https://www.marketwatch.com/rss/topstories",
+    "https://www.pv-magazine.com/feed/",          # Panouri & Baterii
+    "https://3dprintingindustry.com/feed/",       # Imprimare 3D
+    "https://www.technologyreview.com/feed/",      # Inovatii Tech
+    "https://cointelegraph.com/rss"                # Crypto
 ]
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -17,20 +21,21 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 DB_FILE = "stiri_trimise.txt"
 
 def tradu_text(text):
+    if not text or len(text) < 5: return text
     try:
-        # Traduce din orice limbă detectată în Română
-        return GoogleTranslator(source='auto', target='ro').translate(text)
+        # GoogleTranslator are limita de 5000 caractere, deci taiem textul daca e prea lung
+        return GoogleTranslator(source='auto', target='ro').translate(text[:4500])
     except:
-        return text # Dacă traducerea eșuează, trimite textul original
+        return text
 
 def curata_continut(text):
-    if not text:
-        return ""
+    if not text: return ""
     text = re.sub(r'\s+', ' ', text)
+    # Cuvinte de oprire extinse (Romana + Engleza)
     semne_oprire = [
         "Editor :", "Autor :", "Ediția", "Foto:", "Sursa:", 
-        "Redactor:", "Citește și:", "Urmărește-ne și pe",
-        "Copyright ©", "All rights reserved", "Write to ", "Reporting by"
+        "Redactor:", "Citește și:", "Copyright ©", "All rights reserved",
+        "Read more", "Sharing is caring", "Follow us", "Reporting by"
     ]
     for semn in semne_oprire:
         if semn in text:
@@ -38,6 +43,7 @@ def curata_continut(text):
     return text.strip()
 
 def trimite_telegram(titlu, continut, imagine_url):
+    # Pregatim mesajul (Max 1024 caractere pentru poze)
     mesaj_final = f"<b>{titlu}</b>\n\n{continut}"
     if len(mesaj_final) > 1000:
         mesaj_final = mesaj_final[:997] + "..."
@@ -56,24 +62,20 @@ with open(DB_FILE, "r") as f: istoric = f.read().splitlines()
 
 for rss_url in RSS_URLS:
     feed = feedparser.parse(rss_url)
-    for entry in feed.entries[:3]:
+    for entry in feed.entries[:2]: # Luam doar primele 2 stiri din fiecare sursa ca sa nu blocam procesul
         if entry.link not in istoric:
             try:
                 articol = Article(entry.link)
                 articol.download()
                 articol.parse()
                 
-                # Extragem titlul și textul
-                titlu_original = articol.title
-                text_original = curata_continut(articol.text)
+                titlu = articol.title
+                text_complet = curata_continut(articol.text)
                 
-                # Traducem dacă sursa este MarketWatch (sau dacă vrei pentru toate)
-                if "marketwatch.com" in entry.link:
-                    titlu = tradu_text(titlu_original)
-                    text_complet = tradu_text(text_original)
-                else:
-                    titlu = titlu_original
-                    text_complet = text_original
+                # TRADUCERE AUTOMATA: Daca sursa NU este Digi24 sau Hotnews, traduce tot
+                if not any(x in entry.link for x in ["digi24.ro", "hotnews.ro"]):
+                    titlu = tradu_text(titlu)
+                    text_complet = tradu_text(text_complet)
                 
                 poza = articol.top_image
                 trimite_telegram(titlu, text_complet, poza)
